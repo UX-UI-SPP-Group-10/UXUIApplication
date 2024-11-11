5aa89fdf4758bf9779074af6a538d065b9490599
s225042 <144696133+s225042@users.noreply.github.com>
2024-11-11 10:50:56 +0100

    fixst list

--- app/src/main/java/com/group10/uxuiapp/view/ListOverviewPage.kt
+++ app/src/main/java/com/group10/uxuiapp/view/ListOverviewPage.kt
@@ -1,21 +1,30 @@
 package com.group10.uxuiapp.view
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -45,15 +54,22 @@ fun ListOverviewPage(navController: NavController) {
         }
     ) { innerPadding ->
         LazyColumn(contentPadding = innerPadding) {
-            items(100) { index ->
-                Text(
-                    text = "list $index",
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .clickable {
-                            navController.navigate("taskList/$index")
-                        }
-                )
+            items(10) { index ->
+                Box(Modifier.fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(MaterialTheme.colorScheme.secondary)
+                    .clickable {
+                    navController.navigate("taskList/$index")
+                }) {
+                    Text(
+                        text = "list $index",
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                }
+              Spacer(modifier = Modifier.height(8.dp))
             }
         }
     }
