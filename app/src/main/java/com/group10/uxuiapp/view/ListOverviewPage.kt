0eb133f226150bc355b9c4d54dd1d62aa7efc954
Viktor Steffensen <s214707@dtu.dk>
2024-11-11 10:38:54 +0100

    connected changeBar to hold on list

--- app/src/main/java/com/group10/uxuiapp/view/ListOverviewPage.kt
+++ app/src/main/java/com/group10/uxuiapp/view/ListOverviewPage.kt
@@ -1,31 +1,35 @@
 package com.group10.uxuiapp.view
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.uxuiapplication.ChangeButton
 import com.group10.uxuiapp.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListOverviewPage(navController: NavController) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    var selectedIndex by remember { mutableStateOf<Int?>(null) }
 
     Scaffold(
         topBar = {
@@ -45,15 +49,28 @@ fun ListOverviewPage(navController: NavController) {
         }
     ) { innerPadding ->
         LazyColumn(contentPadding = innerPadding) {
-            items(100) { index ->
-                Text(
-                    text = "list $index",
+            items(20) { index ->
+                Column(
                     modifier = Modifier
                         .padding(16.dp)
                         .clickable {
                             navController.navigate("taskList/$index")
                         }
-                )
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onLongPress = {
+                                    selectedIndex = index
+                                }
+                            )
+                        }
+                ) {
+                    Text(text = "List $index")
+
+                    // Show ChangeButton if this item is selected
+                    if (selectedIndex == index) {
+                        ChangeButton(onClose = { selectedIndex = null })
+                    }
+                }
             }
         }
     }
