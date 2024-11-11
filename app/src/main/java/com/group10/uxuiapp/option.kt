52ab79e0f4ff9aa08eaa9e198bad99478893a457
s225042 <144696133+s225042@users.noreply.github.com>
2024-11-11 10:11:27 +0100

    fix

--- app/src/main/java/com/group10/uxuiapp/option.kt
+++ app/src/main/java/com/group10/uxuiapp/option.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.group10.uxuiapp.R
 
 @Composable
 fun ChangeButton() {
