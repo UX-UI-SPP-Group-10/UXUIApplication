d2a049249605279da87a401d111c46d2ae992ef8
karlpedro <162471444+karlpedro@users.noreply.github.com>
2025-01-05 10:36:24 +0100

    flow commpleted

--- app/src/main/java/com/group10/uxuiapp/data/TaskDao.kt
+++ app/src/main/java/com/group10/uxuiapp/data/TaskDao.kt
@@ -11,6 +11,12 @@ interface TaskDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTaskItem(taskItem: TaskItem)
 
+    @Query("SELECT * FROM TaskList WHERE id = :id")
+    fun getTaskListById(id: Int): Flow<TaskList>
+
+    @Query("SELECT * FROM TaskItem WHERE taskListId = :taskListId")
+    fun getTaskItemsByListId(taskListId: Int): Flow<List<TaskItem>>
+
     @Transaction
     @Query("SELECT * FROM TaskList")
     fun getTaskListsWithItems(): Flow<List<TaskListWithItems>>
--- app/src/main/java/com/group10/uxuiapp/data/TaskRepository.kt
+++ app/src/main/java/com/group10/uxuiapp/data/TaskRepository.kt
@@ -6,6 +6,8 @@ class TaskRepository(private val taskDao: TaskDao) {
     suspend fun insertTaskList(taskList: TaskList) = taskDao.insertTaskList(taskList)
     suspend fun insertTaskItem(taskItem: TaskItem) = taskDao.insertTaskItem(taskItem)
     fun getTaskListsWithItems(): Flow<List<TaskListWithItems>> = taskDao.getTaskListsWithItems()
+    fun getTaskListById(id: Int): Flow<TaskList> = taskDao.getTaskListById(id)
+    fun getTaskItemsByListId(taskListId: Int): Flow<List<TaskItem>> = taskDao.getTaskItemsByListId(taskListId)
     suspend fun deleteTaskList(taskList: TaskList) = taskDao.deleteTaskList(taskList)
     suspend fun deleteTaskItem(taskItem: TaskItem) = taskDao.deleteTaskItem(taskItem)
 }
