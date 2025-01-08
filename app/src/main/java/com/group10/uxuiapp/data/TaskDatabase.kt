7588b3af98eed1dfb84ff217228e2bae5d8bc880
karlpedro <162471444+karlpedro@users.noreply.github.com>
2025-01-08 09:15:22 +0100

    comment added

--- app/src/main/java/com/group10/uxuiapp/data/TaskDatabase.kt
+++ app/src/main/java/com/group10/uxuiapp/data/TaskDatabase.kt
@@ -5,7 +5,8 @@ import androidx.room.RoomDatabase
 import com.group10.uxuiapp.data.data_class.TaskItem
 import com.group10.uxuiapp.data.data_class.TodoList
 
-@Database(entities = [TodoList::class, TaskItem::class], version = 5, exportSchema = false)
+// Database class for the TaskList app
+@Database(entities = [TodoList::class, TaskItem::class], version = 5, exportSchema = false) // Increment version number when schema changes
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
