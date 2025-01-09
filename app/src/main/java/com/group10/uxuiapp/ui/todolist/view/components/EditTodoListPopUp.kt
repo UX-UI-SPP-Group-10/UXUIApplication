5c24efe9f63bc0e7abf8effa8050b9eb31790a23
Balder J <baldertjacobsen@gmail.com>
2025-01-09 09:57:41 +0100

    Create the EditTodolistPopup

---
+++ app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/EditTodoListPopUp.kt
@@ -0,0 +1,102 @@
+package com.group10.uxuiapp.ui.todolist.view.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.group10.uxuiapp.R
+
+sealed class EditListPage {
+    object NameInput : EditListPage()
+    object ColorPicker : EditListPage()
+    object DueDatePicker : EditListPage()
+}
+
+
+@Composable
+fun EditTodolistDialog(onDismiss: () -> Unit, onConfirm: (String, String, String) -> Unit) {
+    var currentPage by remember { mutableStateOf<EditListPage>(EditListPage.NameInput) }
+    var listName by remember { mutableStateOf("") }
+    var selectedColor by remember { mutableStateOf("") }
+    var selectedDate by remember { mutableStateOf("") }
+
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = {
+            Row(
+               modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceEvenly
+            ){
+                IconButton(onClick = { currentPage = EditListPage.NameInput }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.edit),
+                        contentDescription = "Edit",
+                        tint = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
+                }
+                IconButton(onClick = { currentPage = EditListPage.ColorPicker }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.palette),
+                        contentDescription = "Color",
+                        tint = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
+                }
+                IconButton(onClick = { currentPage = EditListPage.DueDatePicker }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.calendar),
+                        contentDescription = "Date",
+                        tint = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
+                }
+            }
+        },
+
+        text = {
+            when (currentPage) {
+                is EditListPage.NameInput -> {
+                    Column {
+                        OutlinedTextField(
+                            value = listName,
+                            onValueChange = { listName = it },
+                            label = { Text("List Name") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+                is EditListPage.ColorPicker -> {
+                    // Color Picker Content
+                }
+                is EditListPage.DueDatePicker -> {
+                    // Due Date Picker Content
+                }
+            }
+       },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirm(listName, selectedColor, selectedDate) // Pass the name entered to the onConfirm handler
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
