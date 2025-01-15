5674aafee5754cb979f845b185b613e3026426ba
S235119 <S235119@dtu.dk>
2025-01-15 11:08:22 +0100

    hjerte shadow

--- app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/buttons/IsLikedButton.kt
+++ app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/buttons/IsLikedButton.kt
@@ -1,12 +1,14 @@
 package com.group10.uxuiapp.ui.todolist.view.components.buttons
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,12 +19,36 @@ import com.group10.uxuiapp.data.data_class.TodoList
 fun IsLikedButton(todoList: TodoList, onClick: () -> Unit) {
     val isLiked = todoList.isLiked
 
-    Icon(
-        imageVector = if (isLiked) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-        contentDescription = if (isLiked) "Liked" else "Add Favorite",
+    Box(
         modifier = Modifier
-            .size(25.dp)
-            .clickable { onClick() }, // Use the passed lambda here
-        tint = if (isLiked) Color.Red else Color(android.graphics.Color.parseColor(todoList.textColor))
-    )
+            .size(30.dp)
+            .clickable { onClick() }
+    ) {
+        Icon(
+            imageVector = Icons.Default.FavoriteBorder,
+            contentDescription = "Favorite Outline",
+            modifier = Modifier
+                .size(25.dp)
+                .align(Alignment.Center),
+            tint = Color(android.graphics.Color.parseColor(todoList.textColor))
+        )
+        if (isLiked) {
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = "Favorite Outline",
+                modifier = Modifier
+                    .size(29.dp)
+                    .align(Alignment.Center),
+                tint = Color(0X33000000)
+            )
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Liked",
+                modifier = Modifier
+                    .size(25.dp)
+                    .align(Alignment.Center),
+                tint = Color.Red 
+            )
+        }
+    }
 }
