be4cccdee72fcf067119637e8b35180abe0366c7
s225042 <144696133+s225042@users.noreply.github.com>
2025-01-16 10:51:09 +0100

    delte er flot

--- app/src/main/java/com/group10/uxuiapp/ui/tasks/view/components/buttons/Delete.kt
+++ app/src/main/java/com/group10/uxuiapp/ui/tasks/view/components/buttons/Delete.kt
@@ -1,21 +1,60 @@
 package com.group10.uxuiapp.ui.tasks.view.components.buttons
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.group10.uxuiapp.R
 
 @Composable
 fun Delete(onClick: () -> Unit){
-    Icon(
-        painter = painterResource(id = R.drawable.delete),
-        contentDescription = "Delete",
-        modifier = Modifier.size(20.dp).clickable { onClick() },
-        tint = MaterialTheme.colorScheme.onTertiaryContainer
-    )
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        Button(
+            onClick = onClick,
+            shape = RoundedCornerShape(20),
+            modifier = Modifier
+                .height(48.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFF44336),
+                //containerColor = MaterialTheme.colorScheme.tertiary,
+                contentColor = MaterialTheme.colorScheme.onTertiaryContainer
+            )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.delete),
+                    contentDescription = "Delete",
+                    modifier = Modifier.size(25.dp).offset(x = 12.dp),
+                    tint = MaterialTheme.colorScheme.onTertiaryContainer
+                )
+            }
+        }
+    }
 }
