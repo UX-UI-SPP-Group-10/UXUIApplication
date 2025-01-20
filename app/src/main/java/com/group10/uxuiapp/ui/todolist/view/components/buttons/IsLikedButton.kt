28b858d484e1c4770cfb0325420e6481f925a44c
karlpedro <162471444+karlpedro@users.noreply.github.com>
2025-01-20 14:23:28 +0100

    click outside textbox to save list

--- app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/buttons/IsLikedButton.kt
+++ app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/buttons/IsLikedButton.kt
@@ -2,13 +2,16 @@ package com.group10.uxuiapp.ui.todolist.view.components.buttons
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -16,20 +19,17 @@ import com.group10.uxuiapp.data.data_class.TodoList
 
 
 @Composable
-fun IsLikedButton(todoList: TodoList, onClick: () -> Unit) {
+fun IsLikedButton(todoList: TodoList, onClick: () -> Unit, modifier: Modifier) {
     val isLiked = todoList.isLiked
 
-    Box(
-        modifier = Modifier
-            .size(30.dp)
-            .clickable { onClick() }
+    IconButton(
+        onClick = onClick,
     ) {
         Icon(
             imageVector = Icons.Default.FavoriteBorder,
             contentDescription = "Favorite Outline",
             modifier = Modifier
-                .size(25.dp)
-                .align(Alignment.Center),
+                .size(25.dp),
             tint = Color(android.graphics.Color.parseColor(todoList.textColor))
         )
         if (isLiked) {
@@ -37,16 +37,14 @@ fun IsLikedButton(todoList: TodoList, onClick: () -> Unit) {
                 imageVector = Icons.Default.FavoriteBorder,
                 contentDescription = "Favorite Outline",
                 modifier = Modifier
-                    .size(29.dp)
-                    .align(Alignment.Center),
+                    .size(29.dp),
                 tint = Color(0X33000000)
             )
             Icon(
                 imageVector = Icons.Default.Favorite,
                 contentDescription = "Liked",
                 modifier = Modifier
-                    .size(25.dp)
-                    .align(Alignment.Center),
+                    .size(25.dp),
                 tint = Color.Red
             )
         }
