1f124105f0a574cb28cdf8bf824b7ee4cb557039
S235119 <S235119@dtu.dk>
2025-01-20 11:17:48 +0100

    Bagrundsfarve fikses

--- app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/EditTodoListPopUp.kt
+++ app/src/main/java/com/group10/uxuiapp/ui/todolist/view/components/EditTodoListPopUp.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -249,6 +250,7 @@ fun EditTodolistDialog(
             TextButton(onClick = onDismiss) {
                 Text("Dismiss")
             }
-        }
+        },
+        containerColor = MaterialTheme.colorScheme.tertiary
     )
 }
